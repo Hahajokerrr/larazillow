d7716d638fb4007395b9d1104aca16be418192ab
Phuong <ngotrananhphuongbn@gmail.com>
2023-04-28 17:19:11 +0700

    end sec 8

---
+++ resources/js/Components/ListingSpace.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <span class ="font-bold">{{ listing.beds }} bds </span> <span class = "text-gray-400">|&nbsp;</span>
+
+    <span class ="font-bold">{{ listing.baths }} bds </span> <span class = "text-gray-400">|&nbsp;</span>
+
+    <span class ="font-bold">{{ listing.area }}m²</span>
+
+
+
+</div>
+</template>
+
+<script setup>
+defineProps({
+    listing: Object
+})
+</script>
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,14 +1,36 @@
 <template>
-    <Link href = "/">Main Page</Link>&nbsp
-    <Link href = "/hello">Show Page</Link>
-    <div>The timer {{ timer }}</div>
+    <header class = "border-b border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-900 w-full">
+        <div class = "container mx-auto">
+            <nav class = "p-4 flex items-center justify-between">
+                <div class = "text-lg font-medium">
+                    <Link :href = "route('listing.index')">Listing</Link>
+                </div>
+                <div class = "text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+                    <Link :href = "route('listing.index')">LaraZillow</Link>
+                </div>
+                <div>
+                    <Link :href = "route('listing.create')" class = "btn-primary">+ New Listing</Link>
+
+                </div>
+            </nav>
+        </div>
+    </header>
+
+   <main class = "container mx-auto p-4">
+    <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-greeen-900 bg-green-50 dark:bg-green-900 p-2">
+        {{ flashSuccess }}
+    </div>
     <slot></slot>
+   </main>
 </template>
 
 <script setup>
-import {Link} from '@inertiajs/inertia-vue3'
-import {ref} from 'vue'
+import {computed} from 'vue'
+import {Link, usePage} from '@inertiajs/inertia-vue3'
 
-const timer = ref(0)
-setInterval(() =>timer.value++, 1000)
+const page = usePage()
+const flashSuccess = computed(
+    () => page.props.value.flash.success
+)
 </script>
+
