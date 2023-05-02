c8e4992541cc58f6c6afa1b106dfc8254ac2f7d7
Phuong <ngotrananhphuongbn@gmail.com>
2023-05-02 17:34:15 +0700

    Section12 Done

--- resources/js/Components/ListingSpace.vue
+++ resources/js/Components/ListingSpace.vue
@@ -2,7 +2,7 @@
 <div>
     <span class ="font-bold">{{ listing.beds }} bds </span> <span class = "text-gray-400">|&nbsp;</span>
 
-    <span class ="font-bold">{{ listing.baths }} bds </span> <span class = "text-gray-400">|&nbsp;</span>
+    <span class ="font-bold">{{ listing.baths }} ba </span> <span class = "text-gray-400">|&nbsp;</span>
 
     <span class ="font-bold">{{ listing.area }}m²</span>
 
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,13 +1,23 @@
 <template>
+    <Filters :filters="filters"/>
     <div class = "grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-        <Listing v-for="listing in listings" :key="listing.id" :listing="listing">
+        <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing">
         </Listing>
+    </div>
+
+    <div v-if="listings.data.length" class="w-full flex justify-center mt-4 mb-4">
+        <Pagination :links="listings.links">
+
+        </Pagination>
     </div>
 </template>
 
 <script setup>
+import Pagination from '@/Components/UI/Pagination.vue';
+import Filters from './Index/Components/Filters.vue'
 import Listing from './Index/Components/Listing.vue';
 defineProps({
-    listings: Array,
+    listings: Object,
+    filters: Object,
 })
 </script>
